fd4bf70f9adfa4efd87aec2730ad4717340f19da
ismaeelbashir03 <ismaeelbashir2003@gmail.com>
2025-03-03 21:07:27 +0000

    multicore test

--- task3/runtask.sh
+++ task3/runtask.sh
@@ -20,6 +20,7 @@ CPU_COMBS=(0 1 2 3 0,1 0,2 0,3 1,2 1,3 2,3 0,1,2 0,1,3 0,2,3 1,2,3 0,1,2,3)
 
 for CPU in "${CPU_COMBS[@]}"; do
     echo "Testing CPU combination: $CPU"
+	sleep 10
     
     # If the combination has a comma, split and apply each CPU individually.
     if [[ "$CPU" == *,* ]]; then
@@ -34,7 +35,7 @@ for CPU in "${CPU_COMBS[@]}"; do
     fi
 
     # Monitor schedstat for 10 seconds after applying affinity
-    END=$((SECONDS + 10))
+    END=$((SECONDS + 1))
     while [ $SECONDS -lt $END ]; do
         if [ -e /proc/$PID/schedstat ]; then
             SCHEDSTAT=$(cat /proc/$PID/schedstat 2>/dev/null)
